4f05f53eed16c49f19589de1bff974af780d8d6d
BharatTejK <kbharattej@gmail.com>
2022-04-13 01:43:18 -0400

    Python-04132022

---
+++ PythonVariableNames.py
@@ -0,0 +1,42 @@
+# A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+myvar = "John1"
+my_var = "John2"
+_my_var = "John3"
+myVar = "John4"
+MYVAR = "John5"
+myvar2 = "John6"
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+# Multi Words Variable Names
+# Variable names with more than one word can be difficult to read.
+# There are several techniques you can use to make them more readable:
+
+# Camel Case
+# Each word, except the first, starts with a capital letter:
+myVariableName = "Pawan Kalyan"
+print(myVariableName)
+# Pascal Case
+# Each word starts with a capital letter:
+MyVariableName = "Pawan Kalyan2"
+print(MyVariableName)
+# Snake Case
+# Each word is separated by an underscore character:
+my_variable_name = "Pawan Kalyan3"
+print(my_variable_name)
+
+
+
+
+
+
+
---
+++ PythonVariables.py
@@ -0,0 +1,48 @@
+# Variables are containers for storing data values.
+# Python has no command for declaring a variable.
+# A variable is created the moment you first assign a value to it.
+x = 5
+y = "John"
+print(x)
+print(y)
+#############################################################################
+# Variables do not need to be declared with any particular type, and can even change type after they have been set.
+a = 4         # x is of type int
+a = "Sally"   # x is now of type str
+print(a)
+#############################################################################
+# If you want to specify the data type of a variable, this can be done with CASTING.
+b = str(3)    # x will be '3'
+c = int(3)    # y will be 3
+d = float(3)  # z will be 3.0
+print(b)
+print(c)
+print(d)
+#############################################################################
+# String variables can be declared either by using single or double quotes:
+e = "John"
+print(e)
+#double quotes are the same as single quotes:
+e = 'John'
+print(e)
+#############################################################################
+# Variable names are case-sensitive.
+f = 4
+F = "Sally"
+print(f)
+print(F)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
